f7e1dd1151d4608bed79acecdd13a36b5e27b93e
Kei Ishikawa <Kei Ishikawa>
2017-11-26 22:17:42 +0900

    created simple web server on azure

--- Web_Server/main.py
+++ Web_Server/main.py
@@ -1,9 +1,51 @@
-from flask import Flask
+# Flask などの必要なライブラリをインポートする
+from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+
+# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
+# メッセージをランダムに表示するメソッド
+def picked_up():
+    messages = [
+        "こんにちは、あなたの名前を入力してください",
+        "やあ！お名前は何ですか？",
+        "あなたの名前を教えてね"
+    ]
+    # NumPy の random.choice で配列からランダムに取り出し
+    return np.random.choice(messages)
+
+# ここからウェブアプリケーション用のルーティングを記述
+# index にアクセスしたときの処理
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    title = "ようこそ"
+    message = picked_up()
+    # index.html をレンダリングする
+    return render_template('index.html', message=message, title=title)
+
+# /post にアクセスしたときの処理
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    title = "こんにちは"
+    if request.method == 'POST':
+        # リクエストフォームから「名前」を取得して
+        name = request.form['name']
+        # index.html をレンダリングする
+        return render_template('index.html', name=name, title=title)
+    else:
+        # エラーなどでリダイレクトしたい場合はこんな感じで
+        return redirect(url_for('index'))
+
+@app.route('/hey')
+def hey():
+	return "hey"
+
+@app.route('/bar_chart')
+def bar_chart():
+	return render_template('bar_chart.html')
 
 if __name__ == '__main__':
-  app.run()
+    app.debug = True # デバッグモード有効化
+    app.run(host='0.0.0.0') # どこからでもアクセス可能に
+    
